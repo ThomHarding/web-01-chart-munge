a6a867e32b3ca011481296db8fbe4c1effa91451
THOMAS HARDING <hardingt604@gmail.com>
2022-04-01 14:30:15 -0600

    added variety of colours to gender chart

--- app.js
+++ app.js
@@ -1,56 +1,77 @@
 import { exportData } from './data.js';
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-let dataMap = [];
+let purchaseFreqMap = [];
+let coolFactorMap = [];
+let genderMap = [];
+
 for (let data of exportData) {
-    if (dataMap[data.purchase_frequency]) {
-        dataMap[data.purchase_frequency]++;
+    if (purchaseFreqMap[data.purchase_frequency]) {
+        purchaseFreqMap[data.purchase_frequency]++;
     } else {
-        dataMap[data.purchase_frequency] = 1;
+        purchaseFreqMap[data.purchase_frequency] = 1;
+    }
+    if (coolFactorMap[data.cool_factor]) {
+        coolFactorMap[data.cool_factor]++;
+    } else {
+        coolFactorMap[data.cool_factor] = 1;
+    }
+    if (genderMap[data.gender]) {
+        genderMap[data.gender]++;
+    } else {
+        genderMap[data.gender] = 1;
     }
 }
 
-console.log(dataMap);
+console.log(coolFactorMap);
 
 const dataOne = {
-    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    labels: ['Daily', 'Monthly', 'Never', 'Often', 'Once', 'Seldom', 'Weekly', 'Yearly'],
     datasets: [{
         label: 'Customers by Purchase Frequency',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: exportData,
-        parsing: {
-            yAxisKey: 'purchase_frequency'
-        }
+        data: purchaseFreqMap
     }]
 };
 
 const dataTwo = {
-    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     datasets: [{
-        label: 'Customers by Purchase Frequency',
+        label: 'Customers by Cool Factor',
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgb(255, 99, 132)',
-        data: exportData['purchase_frequency'],
+        data: coolFactorMap,
     }]
 };
 
 const dataThree = {
-    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    labels: [
+        'Agender',
+        'Bigender',
+        'Female',
+        'Genderfluid',
+        'Genderqueer',
+        'Male',
+        '"Non-binary"',
+        'Polygender',
+    ],
     datasets: [{
-        label: 'Customers by Purchase Frequency',
-        backgroundColor: 'rgb(255, 99, 132)',
+        label: 'Customers by Gender',
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(0, 99, 132)',
+            'rgb(255, 255, 132)',
+            'rgb(255, 0, 132)',
+            'rgb(255, 99, 0)',
+            'rgb(255, 99, 255)',
+            'rgb(0, 0, 132)',
+            'rgb(255, 0, 0)'
+        ], 
         borderColor: 'rgb(255, 99, 132)',
-        data: exportData,
+        data: genderMap,
     }]
 };
+
 
 const configOne = {
     type: 'line',
@@ -69,7 +90,6 @@ const configThree = {
     data: dataThree,
     options: {}
 };
-
 
 const chartOne = new Chart(
     document.getElementById('custByPurchFreqBar'),
