52d1e66d013d104e851a091288a5333c45995e3b
THOMAS HARDING <hardingt604@gmail.com>
2022-04-01 14:15:45 -0600

    commit to show work. chart.js isn't reading exportData right

--- app.js
+++ app.js
@@ -1,8 +1,85 @@
-// import functions and grab DOM elements
+import { exportData } from './data.js';
 
-// let state
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+];
+let dataMap = [];
+for (let data of exportData) {
+    if (dataMap.purchase_frequency) {
+        dataMap.purchase_frequency++;
+    } else {
+        dataMap.purchase_frequency = 1;
+    }
+}
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const dataOne = {
+    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    datasets: [{
+        label: 'Customers by Purchase Frequency',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: exportData,
+        parsing: {
+            yAxisKey: 'purchase_frequency'
+        }
+    }]
+};
+
+const dataTwo = {
+    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    datasets: [{
+        label: 'Customers by Purchase Frequency',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: exportData['purchase_frequency'],
+    }]
+};
+
+const dataThree = {
+    labels: ['Once', 'Seldom', 'Often', 'Daily', 'Weekly', 'Monthly', 'Yearly'],
+    datasets: [{
+        label: 'Customers by Purchase Frequency',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: exportData,
+    }]
+};
+
+const configOne = {
+    type: 'line',
+    datasets: dataOne,
+    options: {}
+};
+
+const configTwo = {
+    type: 'bar',
+    data: dataTwo,
+    options: {}
+};
+
+const configThree = {
+    type: 'pie',
+    data: dataThree,
+    options: {}
+};
+
+
+const chartOne = new Chart(
+    document.getElementById('custByPurchFreqBar'),
+    configOne
+);
+
+const chartTwo = new Chart(
+    document.getElementById('custByCoolFactorLine'),
+    configTwo
+);
+
+const chartThree = new Chart(
+    document.getElementById('custByGenderPie'),
+    configThree
+);
